f81a1d12478b64be95b355a7c7973b941c7556c8
dominiclovell <dom@domlovell.com>
2014-12-23 14:17:19 +1100

    Update miro.rb

--- lib/miro.rb
+++ lib/miro.rb
@@ -4,13 +4,16 @@ require "cocaine"
 require "color"
 require "tempfile"
 require "open-uri"
-
+require 'fileutils'
+require 'net/https'
+require 'tmpdir'
+require 'digest/sha1'
 require "miro/dominant_colors"
 
 module Miro
   class << self
     def options
-      convert = `which convert`.strip
+      convert = '' #`which convert`.strip
       @options ||= {
         :image_magick_path  => convert.length > 0 ? convert : '/usr/bin/convert',
         :resolution         => "150x150",
@@ -27,4 +30,19 @@ module Miro
       options[:method] == 'histogram'
     end
   end
+end
+
+module Kernel
+  if !self.methods.include?(:silence_warnings)
+    def silence_warnings
+      old_verbose, $VERBOSE = $VERBOSE, nil
+      yield
+    ensure
+      $VERBOSE = old_verbose
+    end
+  end
+end
+
+silence_warnings do
+	OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 end
